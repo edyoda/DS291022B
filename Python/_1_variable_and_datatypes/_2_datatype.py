b565823ec818e5012742e12c4f7463f351225581
Sherly14 <bharatiyadav14@gmail.com>
2022-11-03 21:28:31 +0530

    initial

---
+++ Python/_1_variable_and_datatypes/_2_datatype.py
@@ -0,0 +1,36 @@
+# Datatypes
+
+# int - digit
+# float - decimal
+# str - word,character,statment,para
+# bool - boolean (True(1)/False(0))
+# complex - real + imaginary 
+# NoneType - void
+
+# list
+# tuple
+# set
+# dict
+
+num = 2
+print(type(num))
+
+salary = 15000.50
+salary_type = type(salary)
+print(salary_type)
+
+name = "Bharati"
+name_type = type(name)
+print(name_type)
+
+female = True
+female_type = type(female)
+print(female_type)
+
+data = 2+1j
+data_type = type(data)
+print(data_type)
+
+hobby = None
+hobby_type = type(hobby)
+print(hobby_type)
